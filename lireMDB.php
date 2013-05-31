27a8a6868d8d04348ed3e727c8237500f329f162
root <root@debianSiSRProf.btsinfogap.org>
2013-05-31 14:07:49 +0200

    envois des données

---
+++ lireMDB.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<title>Liste des visiteurs</title>
+</head>
+<body>
+	<table>
+	<thead>
+		<tr>
+			<th>Matricule</th>
+			<th>Nom</th>
+			<th>Prenom</th>
+			<th>Adresse</th>
+			<th>Code Postal</th>
+			<th>Ville</th>
+			<th>Date embauche</th>
+		</tr>
+		<?
+		$link	
+		?>
+	</thead>
+	<tbody>
+	</tbody>
+	</table>
+</body>
+</html>
